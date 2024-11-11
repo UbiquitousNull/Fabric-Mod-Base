5353736ed3ff3964433cdd44653d41bb81c7e178
Jonathan Merrill <99621514+UbiquitousNull@users.noreply.github.com>
2024-11-12 08:22:30 +1300

    Add files via upload

---
+++ src/main/java/ubiquitousnull/modbase/ModBaseDataGenerator.java
@@ -0,0 +1,11 @@
+package ubiquitousnull.modbase;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModBaseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
