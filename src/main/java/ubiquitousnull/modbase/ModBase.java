d32b2f96e6f9ff1b161530594f9231dd719ab62c
Jonathan <99621514+UbiquitousNull@users.noreply.github.com>
2024-11-16 13:19:19 +1300

    Set to Base.

--- src/main/java/ubiquitousnull/modbase/ModBase.java
+++ src/main/java/ubiquitousnull/modbase/ModBase.java
@@ -12,8 +12,5 @@ public class ModBase implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 	}
 }
